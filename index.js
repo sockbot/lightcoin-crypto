82559c70b3fc9767f2606159810c2afef8e7d1d4
sockbot <github@sockbot.com>
2019-08-03 00:27:52 +0000

    added transaction tracking and made balance a calculated value

--- index.js
+++ index.js
@@ -1,29 +1,87 @@
-let balance = 500.00;
+// let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
+  constructor(amount, account) {
+    this._amount = amount;
+    this.account = account;
+  }
+  commit() {
+    // const isAllowed = function(transaction) {
+    //   console.log("Transaction:", transaction);
+    //   console.log("Balance:", this);
+    //   // if (transaction.value > this.balance) {
+    //   //   return false;
+    //   // }
+    //   return true;
+    // };
+    // console.log("This.value:", this.value);
+    // if (isAllowed(this.value)) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    // }
+    // console.log("Transaction failed. Account balance is", this.account.balance);
+  }
+}
 
-  constructor(amount) {
-    this.amount = amount;
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // account balance starts at 0
+    this.transactions = [];
+    // this._balance = this.balance
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    // calculate the balance using the transaction objects.
+    return this.transactions.reduce((total, transaction) => {
+      // console.log("Transaction:", transaction)
+      return total + transaction.value;
+    }, 0);
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return this._amount * -1;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this._amount;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+console.log(myAccount);
+
+
+let t1 = new Withdrawal(10, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+// console.log(myAccount)
+// console.log("Balance:", myAccount.balance)
+let t2 = new Withdrawal(10, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+let t3 = new Deposit(30.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+console.log('Balance:', myAccount.balance);
